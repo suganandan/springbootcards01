8c0d80614f32a24fe61a440e6dfc010e40137505
suganandan <suganandan@hotmail.com>
2024-10-05 19:31:00 +0530

    New Files

---
+++ src/main/java/com/demo01/springbootcards01/Springbootcards01Application.java
@@ -0,0 +1,13 @@
+package com.demo01.springbootcards01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootcards01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springbootcards01Application.class, args);
+	}
+
+}
